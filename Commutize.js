21f3e05f8b0a954df9fd57cfcc8b926372ae038f
Jason Lee <jason.ferrier+github@gmail.com>
2013-07-17 09:56:39 -0700

    Create Commutize.js

---
+++ Commutize.js
@@ -0,0 +1,3 @@
+/* This Bookmarklet will mark all of the rides on the multi-event edit page (the step after uploading) as Commutes */
+/* Add a new bookmark to your browser and paste the following line in the URL or Address field */
+javascript:(function()%7BjQuery('%5Bname%3D%22commute%22%5D').attr('checked'%2C%20true)%7D)()
