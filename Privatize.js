c5157880dd9edfdeee26473a0e5e491a2e372075
Jason Lee <jason.ferrier+github@gmail.com>
2013-07-17 09:56:20 -0700

    Update Privatize.js
    Fixed typo.

--- Privatize.js
+++ Privatize.js
@@ -1,3 +1,3 @@
 /* This Bookmarklet will mark all of the rides on the multi-event edit page (the step after uploading) as Private */
-/* Add a new bookmark to your browser and paste the folling line in the URL or Address field */
+/* Add a new bookmark to your browser and paste the following line in the URL or Address field */
 javascript:(function()%7BjQuery('.privacy-button').addClass('active')%3BjQuery('.private-icon').addClass('disabled')%7D)()
